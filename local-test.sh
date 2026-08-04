dbc1624da927ec015ee0ed4d3b63941ffae430f0
Matthew Grogan <matthew.grogan@mongodb.com>
2026-08-04 15:13:20 -0400

    improved test script

--- local-test.sh
+++ local-test.sh
@@ -2,13 +2,19 @@
 
 if [ "$1" == "start" ]; then
     # Run mongodb enterprise container
-    podman run quay.io/mongodb/mongodb-enterprise:8.3.7 --name mongodb-enterprise -d -p 27017:27017
+    podman run quay.io/mongodb/mongodb-enterprise:8.3.7 --name mongodb-enterprise -d -p 27017:27017 -e MONGO_INITDB_ROOT_USERNAME=root -e MONGO_INITDB_ROOT_PASSWORD=password
+
+    # Write root password file to be mounted in mongot
+    echo "password" > ./mongodb-enterprise-root-password
 
     # Run mongodb search container
-    podman run mongodb-search:1.70.1 --name mongodb-search -d -p 27028:27028
+    podman run mongodb-search:1.70.1 --name mongodb-search -d -p 27028:27028 \
+    -v $(pwd)/mongodb-enterprise-root-password:/tmp/mongodb-enterprise-root-password \
+    -v $(pwd)/local-test-config.yaml:/mongot-community/config.default.yml
 
 elif [ "$1" == "stop" ]; then
     # Stop and delete containers
     podman stop mongodb-enterprise mongodb-search
     podman rm mongodb-enterprise mongodb-search
+    rm ./mongodb-enterprise-root-password
 fi
